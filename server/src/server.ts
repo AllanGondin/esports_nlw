46440dc0ee721a321f763695269a9486216fe63c
AllanGondin <allangondin@gmail.com>
2022-09-20 16:54:56 -0400

    first commit|initialized Home web and mobile

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+app.get("/ads", (request, response) => {
+  return response.json([
+    { id: "foi", name: "foiTbm" },
+    { id: "foi", name: "foiTbm" },
+    { id: "foi", name: "foiTbm" },
+    { id: "foi", name: "foiTbm" },
+  ]);
+});
+
+app.listen(3333);
