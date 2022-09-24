456393c4756c201178246764d5f3fb363e7e2e06
AllanGondin <allangondin@gmail.com>
2022-09-24 18:44:23 -0400

    feat: Finished home Screen

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,13 +1,27 @@
-import { View, Image, FlatList } from 'react-native';
+import {useEffect, useState} from 'react'
+
+import { Image, FlatList } from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context'
+
 import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
-import {GAMES} from '../../utils/games';
+
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(()=>{
+    fetch('http://10.0.0.250:3333/games')
+    .then(response => response.json())
+    .then(data => 
+      setGames(data)
+    )
+  },[])
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image 
       source={logoImg}
       style={styles.logo}
@@ -18,7 +32,7 @@ export function Home() {
         subtitle= "Selecione o game que deseja jogar..."
       />
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({item})=>(
           <GameCard
@@ -29,6 +43,6 @@ export function Home() {
         showsHorizontalScrollIndicator = {false}
         horizontal
       />
-    </View>
+    </SafeAreaView>
   );
 }
